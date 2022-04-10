50e602a9bfe0c2640b961ef886f12c50ebc928dd
ColonelBucket8 <freedomterran89@gmail.com>
2022-04-10 16:07:37 +0800

    updated awesome config

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -8,3 +8,6 @@ function run {
 run "fcitx5"
 run "picom"
 run "nm-applet"
+run "xrandr --output DVI-D-0 --mode 1920x1080 --rate 120.00 --auto --left-of HDMI-0"
+run "xautolock -detectsleep -time 10 -locker "i3lock -c 000000"
+
