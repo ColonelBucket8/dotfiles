85a71b4dcef245c34bc2f7a6d67a038dfa90ddb3
ColonelBucket8 <freedomterran89@gmail.com>
2022-05-08 20:31:36 +0800

    added kvm windows 10 single gpu passthrough config

---
+++ kvm/qemu.d/win10/prepare/begin/start.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Helpful to read output when debugging
+set -x
+
+## Load the config file with our environmental variables
+source "/etc/libvirt/hooks/kvm.conf"
+
+# Stop display manager (KDE specific)
+systemctl stop display-manager
+
+# Unbind VTconsoles
+echo 0 > /sys/class/vtconsole/vtcon0/bind
+echo 0 > /sys/class/vtconsole/vtcon1/bind
+
+# Unbind EFI-Framebuffer
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+# Avoid a race condition
+sleep 5
+
+# Isolate cpu
+systemctl set-property --runtime -- system.slice AllowedCPUs=0,1
+systemctl set-property --runtime -- user.slice AllowedCPUs=0,1
+systemctl set-property --runtime -- init.scope AllowedCPUs=0,1
+
+# Unload all Nvidia drivers
+modprobe -r nvidia_drm
+modprobe -r nvidia_uvm
+modprobe -r nvidia_modeset
+modprobe -r drm_kms_helper
+modprobe -r nvidia
+modprobe -r i2c_nvidia_gpu
+modprobe -r drm
+
+# Unbind the GPU from display driver
+virsh nodedev-detach $VIRSH_GPU_VIDEO
+virsh nodedev-detach $VIRSH_GPU_AUDIO
+
+# Load VFIO kernel module
+modprobe vfio
+modprobe vfio_pci
+modprobe vfio_iommu_type1
