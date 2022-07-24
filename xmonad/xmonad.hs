1b4887ed5295b7462ab1574dbc86de01881b0f89
ColonelBucket8 <freedomterran89@gmail.com>
2022-07-24 17:44:20 +0800

    added xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,10 +1,28 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Layout.ThreeColumns
+import XMonad.Hooks.EwmhDesktops
 
-main = xmonad def
+-- The main function
+main :: IO ()
+main = xmonad . ewmhFullscreen . ewmh . xmobarProp $ myConfig
+
+-- Command to launch the bar
+myBar = "xmobar"
+
+myPP = xmobarPP 
+    { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- toggleStrutsKey XConfig {XMonad.modMask = modMask} = {modMask, xK_b}
+
+myConfig = def
     { terminal    = myTerminal
     , modMask     = myModMask
     , borderWidth = myBorderWidth
-    , layoutHook  = myLayout
+    -- , layoutHook  = myLayout
     , focusFollowsMouse = myFocusFollowsMouse
     }
 
@@ -12,10 +30,10 @@ myTerminal :: String
 myTerminal = "alacritty"
 
 myFocusFollowsMouse :: Bool
-myFocusFollowsMouse = true
+myFocusFollowsMouse = True
 
-myBorderWidth :: Int
-myBorderWidth = 3
+myBorderWidth :: Dimension
+myBorderWidth = 2
 
 myModMask = mod4Mask
-myLayout = spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True $ Tall (1 (3/100) (1/2)) ||| Full
+-- myLayout = spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True $ Tall (1 (3/100) (1/2)) ||| Full
