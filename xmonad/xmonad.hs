5522edcb6d8e441cbfce796bc5821c636a6bc8c0
ColonelBucket8 <freedomterran89@gmail.com>
2022-08-06 06:48:13 +0800

    added xmonad configs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,9 +2,15 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
-import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.EZConfig (additionalKeys,removeKeys)
 import XMonad.Layout.ThreeColumns
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.Renamed
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.LayoutModifier(ModifiedLayout)
+import XMonad.Actions.UpdatePointer
 
 -- The main function
 main :: IO ()
@@ -22,9 +28,11 @@ myConfig = def
     { terminal    = myTerminal
     , modMask     = myModMask
     , borderWidth = myBorderWidth
-    -- , layoutHook  = myLayout
+    , layoutHook  = myLayout
     , focusFollowsMouse = myFocusFollowsMouse
-    }
+    , startupHook = myStartupHook
+    , logHook     = myLogHook
+    } `removeKeys` myRemoveKeys `additionalKeys` myKeys
 
 myTerminal :: String
 myTerminal = "alacritty"
@@ -35,5 +43,38 @@ myFocusFollowsMouse = True
 myBorderWidth :: Dimension
 myBorderWidth = 2
 
+-- Mod key to Win key
 myModMask = mod4Mask
--- myLayout = spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True $ Tall (1 (3/100) (1/2)) ||| Full
+
+-- Add gaps
+myLayout = mySmartSpacing $ layoutTall ||| layoutFull
+    where
+        myGap = spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True
+        mySmartSpacing = smartSpacing 5
+        layoutTall = Tall 1 (3/100) (1/2)
+        layoutFull = Full
+
+-- Remove default keybinding
+myRemoveKeys = 
+ [ ((myModMask .|. shiftMask, xK_Return))]
+
+-- Custom keybinding
+myKeys =
+ [ ((myModMask, xK_f), spawn "firefox")
+ , ((myModMask, xK_Return), spawn "alacritty")
+ , ((myModMask, xK_p), spawn "rofi -show drun -show-icons")
+--  , ((myModMask .|. shiftMask, xK_Return), spawn "steam")
+ ]
+
+-- Cursor follows focus window
+myLogHook = updatePointer (0.5, 0.5) (0, 0)
+
+-- Autostart programs
+myStartupHook = do
+    spawn "feh --bg-scale ~/Pictures/Wallpapers/fate-saber-2.jpg"
+    spawn "picom"
+    spawn "fcitx5"
+    spawn "nm-applet"
+    spawn "xrandr --output DVI-D-0 --mode 1920x1080 --rate 120.00 --auto --left-of HDMI-0 --output HDMI-0"
+    spawn "xautolock -detectsleep -time 10 -locker 'i3lock -c 000000'"
+    spawn "xmobar -x 1 ~/.xmobarrc"
