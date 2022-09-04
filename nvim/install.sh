28c3686ba3531e167069bf699ea16c888b9eb973
ColonelBucket8 <freedomterran89@gmail.com>
2022-09-04 21:33:54 +0800

    edited install script neovim

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,6 +3,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 mkdir -p ~/.config/nvim
-cp -r ./* ~/.config/nvim
+cp ./plugins.vim ~/.config/nvim 
+echo "so~/.config/nvim/plugins.vim" > ~/.config/nvim/init.vim
 nvim +PlugInstall +qa
+cp -r ./* ~/.config/nvim
 
