0c0c18905ad60bdacb4e82fc1b63e8ff5d5b2b24
ColonelBucket8 <freedomterran89@gmail.com>
2022-09-07 02:26:03 +0800

    added window swallowing

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,14 +3,17 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
-import XMonad.Util.EZConfig (additionalKeys,removeKeys)
-import XMonad.Layout.ThreeColumns
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.WindowSwallowing
+
 import XMonad.Layout.Renamed
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.LayoutModifier(ModifiedLayout)
+
+import XMonad.Util.EZConfig (additionalKeys,removeKeys)
 import XMonad.Actions.UpdatePointer
 
 -- The main function
@@ -34,6 +37,7 @@ myConfig = def
     , startupHook = myStartupHook
     , logHook     = myLogHook
     , manageHook  = myManageHook
+    , handleEventHook = myHandleEventHook
     } `removeKeys` myRemoveKeys `additionalKeys` myKeys
 
 myTerminal :: String
@@ -54,7 +58,7 @@ myLayout = avoidStruts $ myGap $ layoutTall ||| layoutFull
         myGap = spacingRaw False (Border 5 0 5 0) True (Border 0 5 0 5) True
         mySpacing = spacing 5
         mySmartSpacing = smartSpacing 5
-        layoutTall = spacing 3 $ Tall 1 (3/100) (1/2)
+        layoutTall = renamed [Replace "Tall"] $ spacing 3 $ Tall 1 (3/100) (1/2)
         layoutFull = noBorders Full
 
 -- Remove default keybinding
@@ -75,17 +79,20 @@ myLogHook = updatePointer (0.5, 0.5) (0, 0)
 
 myManageHook = manageHook def <+> manageDocks
 
+-- Window Swallowing
+myHandleEventHook = swallowEventHook (className =? "Alacritty" <||> className =? "XTerm") (return True)
+
 -- Autostart programs
 myStartupHook = do
     spawn "feh --bg-scale ~/Pictures/Wallpapers/anime-4.jpg"
     spawn "picom --experimental-backends"
     spawn "fcitx5"
     -- spawn "nm-applet"
-    -- spawn "xrandr --output DVI-D-0 --mode 1920x1080 --rate 120.00 --auto --left-of HDMI-0 --output HDMI-0"
+    spawn "xrandr --output DVI-D-0 --mode 1920x1080 --rate 120.00 --auto --left-of HDMI-0 --output HDMI-0"
     spawn "xautolock -detectsleep -time 10 -locker 'i3lock -c 000000'"
     -- Lockscreen with background image, png only
     -- spawn "xautolock -detectsleep -time 10 -locker 'i3lock -i ~/Pictures/Wallpapers/jabami-yumeko.png"
     spawn "xmobar -x 1 ~/.xmobarrc"
-    spawn "conky"
+    -- spawn "conky"
     -- System tray
     -- spawn "stalonetray"
