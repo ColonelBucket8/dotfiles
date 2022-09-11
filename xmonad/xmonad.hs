7e87494e1c16d8aacd5baa9b6c2a12761245dade
ColonelBucket8 <freedomterran89@gmail.com>
2022-09-11 23:53:59 +0800

    edited xmonad workspace

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
@@ -13,20 +14,49 @@ import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.LayoutModifier(ModifiedLayout)
 
+import XMonad.Util.Loggers
 import XMonad.Util.SpawnOnce
 import XMonad.Util.EZConfig (additionalKeys,removeKeys)
+
 import XMonad.Actions.UpdatePointer
 
 -- The main function
 main :: IO ()
-main = xmonad . ewmhFullscreen . ewmh . xmobarProp $ myConfig
+main = xmonad 
+     . ewmhFullscreen 
+     . ewmh 
+     . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
+     $ myConfig
 
 -- Command to launch the bar
 myBar = "xmobar"
 
-myPP = xmobarPP 
-    { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+myXmobarPP :: PP
+myXmobarPP = def 
+    { ppSep             = magenta " | "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap "|" "|" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where 
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
 
+
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
 -- toggleStrutsKey XConfig {XMonad.modMask = modMask} = {modMask, xK_b}
 
 myConfig = def
