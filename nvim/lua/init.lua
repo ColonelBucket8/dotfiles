8de2bf099c9daa7c68cb518c562bf653c71899a2
ColonelBucket8 <freedomterran89@gmail.com>
2022-09-17 23:02:18 +0800

    added neovim dashboard

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,3 +1,4 @@
+require('config.tokyonight')
 require('options')
 require('config.treesitter')
 require('config.lsp')
@@ -5,4 +6,12 @@ require('config.lspsaga')
 require('config.cmp')
 require('nvim-tree').setup()
 require('config.lualine')
+require('config.dap')
+require('config.dashboard')
+-- require('null-ls').setup({
+--   sources = {
+--     require('null-ls').builtins.formatting.prettier
+--   }
+-- })
+-- require('which-key').setup()
 require('keybindings')
