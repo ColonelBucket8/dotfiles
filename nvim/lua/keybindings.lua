5882cb8d6ca5a0516af79913b3aea6eebca5fa2d
ColonelBucket8 <freedomterran89@gmail.com>
2022-09-18 01:18:26 +0800

    changed keybindings

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -2,12 +2,32 @@ vim.g.mapleader = ' '
 local keymap = vim.keymap.set
 
 -- Normal mode
-keymap('n', '<Leader>s', "<Cmd>update<CR>")
-keymap('n', '<C-a>', "<Cmd>NvimTreeToggle<CR>")
-keymap('n', '<C-f>', "<Cmd>NvimTreeFindFile<CR>")
-keymap('n', '<C-p>', "<Cmd>Files<CR>")
-keymap('n', '<Leader>f', "<Cmd>Rg<CR>")
-keymap('n', '<Leader>p', "<Cmd>PrettierAsync<CR>")
+-- File
+keymap('n', '<Leader>fs', "<Cmd>update<CR>")
+
+-- Project
+keymap('n', '<Leader>pf', "<Cmd>Files<CR>")
+keymap('n', '<Leader>ps', "<Cmd>Rg<CR>")
+keymap('n', '<Leader>pg', "<Cmd>GFiles<CR>")
+
+-- Open
+keymap('n', '<Leader>os', "<Cmd>NvimTreeToggle<CR>")
+keymap('n', '<Leader>oc', "<Cmd>NvimTreeFindFile<CR>")
+keymap('n', '<Leader>of', "<Cmd>NvimTreeFocus<CR>")
+keymap('n', '<Leader>ot', "<Cmd>term<CR>")
+
+-- Search
+keymap('n', '<Leader>sc', "<Cmd>Commands<CR>")
+
+-- Code
+keymap('n', '<Leader>cp', "<Cmd>PrettierAsync<CR>")
+
+-- Buffer
+keymap('n', '<Leader>bf', "<Cmd>Buffers<CR>")
+keymap('n', '<Leader>bd', "<Cmd>bd<CR>")
+
+-- Git
+keymap('n', '<Leader>gd', "<Cmd>Gvdiffsplit<CR>")
 
 -- Insert mode
 keymap('i', 'kj', "<Esc>")
