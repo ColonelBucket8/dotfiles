23ce0a67311046f4fe1c35fae39df84488aebb6e
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-10-15 00:30:22 +0800

    added truncate path display option for telescope

--- nvim/windows/lua/init.lua
+++ nvim/windows/lua/init.lua
@@ -1,13 +1,19 @@
-require('options')
-require('config.treesitter')
-require('config.lsp')
-require('config.lspsaga')
-require('config.cmp')
-require('config.nvimtree')
-require('config.lualine')
-require('config.dashboard')
-require('config.null-ls')
-require('nvim-autopairs').setup()
-require('keybindings')
-
+require("options")
+require("config.treesitter")
+require("config.lsp")
+require("config.lspsaga")
+require("config.cmp")
+require("config.nvimtree")
+require("config.lualine")
+require("config.dashboard")
+require("config.null-ls")
+require("nvim-autopairs").setup()
+require("telescope").setup({
+	defaults = {
+		path_display = {
+			truncate = 3,
+		},
+	},
+})
+require("keybindings")
 -- require('bufferline').setup()
