ae6830f2eec93c7e8455cdfc8199410eed8508ba
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-11-06 00:42:42 +0800

    added pcall for safely call module

--- nvim/windows/lua/config/cmp.lua
+++ nvim/windows/lua/config/cmp.lua
@@ -1,11 +1,15 @@
-local cmp = require("cmp")
-local luasnip = require("luasnip")
+local present_cmp, cmp = pcall(require, "cmp")
+local present_luasnip, luasnip = pcall(require, "luasnip")
+
+if not present_cmp or not present_luasnip then
+	return
+end
 
 cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+			luasnip.lsp_expand(args.body) -- For `luasnip` users.
 			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
