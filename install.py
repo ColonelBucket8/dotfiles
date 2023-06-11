838e1cf542bb67c3a02102226e20086f402ebc08
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-06-11 23:58:58 +0800

    Update install

--- install.py
+++ install.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+from sys import platform
 
 def install_tmux():
     file = open("tmux/tmux.conf","r")
@@ -7,17 +9,26 @@ def install_tmux():
     
     is_dir_exist = os.path.isdir(os.path.join(os.path.expanduser("~"), ".config", "tmux"))
 
-    file_path = os.path.join(os.path.expanduser("~"), ".config", "tmux", "tmux.conf")
+    dir_path = os.path.join(os.path.expanduser("~"), ".config", "tmux")
+    file_path = os.path.join(dir_path, "tmux.conf")
 
     if (is_dir_exist):
         file_create = open(file_path, "w")
         file_create.write(lines)
     else:
-        os.makedirs(os.path.join(os.path.expanduser("~"), ".config", "tmux"))
+        os.makedirs(dir_path)
         file_create = open(file_path, "w")
         file_create.write(lines)
         file_create.close()
 
-install_tmux()
+    subprocess.run(f"git clone https://github.com/tmux-plugins/tpm {os.path.expanduser('~')}/.tmux/plugins/tpm")
+
+if platform == "linux" or platform == "darwin":
+    install_tmux()
+# elif os.system.platform == "darwin":
+    # OS X
+# elif os.system.platform == "win32":
+    # Windows...
+
 
 
