d126a4095d76d184fca5350a90ced5223ca1a593
ColonelBucket8 <freedomterran89@gmail.com>
2023-09-01 01:05:04 +0800

    Add mappings

--- nvim-chad/mappings.lua
+++ nvim-chad/mappings.lua
@@ -1,25 +1,32 @@
 ---@type MappingsTable
 local M = {}
 
+M.disabled = {
+	n = {
+		["<leader>h"] = "",
+		["<leader>b"] = "",
+	},
+}
+
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+	},
 }
 
 M.custom = {
-  n = {
-    ["<leader>pf"] = { ":Telescope find_files <CR>", "Telescope find files" },
-    ["<leader>op"] = { ":NvimTreeToggle <CR>", "Toggle nvim tree" },
-    ["<leader>oP"] = { ":NvimTreeToggle <CR>", "Focus nvim tree" },
-    ["<leader>/"] = { ":Telescope live_grep<CR>", "Search keywords in project" },
-    ["<leader>bi"] = { ":Telescope buffers<CR>", "Search buffers" },
-    ["<leader>gg"] = { ":LazyGit<CR>", "Lazygit" },
-  },
-  v = {
-    ["<"] = { "<gv" },
-    [">"] = { ">gv" },
-  },
+	n = {
+		["<leader>pf"] = { ":Telescope find_files <CR>", "Telescope find files" },
+		["<leader>op"] = { ":NvimTreeToggle <CR>", "Toggle nvim tree" },
+		["<leader>oP"] = { ":NvimTreeToggle <CR>", "Focus nvim tree" },
+		["<leader>/"] = { ":Telescope live_grep<CR>", "Search keywords in project" },
+		["<leader>bi"] = { ":Telescope buffers<CR>", "Search buffers" },
+		["<leader>gg"] = { ":LazyGit<CR>", "Lazygit" },
+	},
+	v = {
+		["<"] = { "<gv" },
+		[">"] = { ">gv" },
+	},
 }
 
 -- more keybinds!
