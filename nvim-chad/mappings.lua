f24f99aa20176ce6f38c11ea103b55194f1d9aea
ColonelBucket8 <freedomterran89@gmail.com>
2023-09-01 02:45:03 +0800

    Add keymaps

--- nvim-chad/mappings.lua
+++ nvim-chad/mappings.lua
@@ -18,8 +18,20 @@ M.general = {
 M.custom = {
 	n = {
 		["<leader>pf"] = { ":Telescope find_files <CR>", "Telescope find files" },
+		["gr"] = {
+			function()
+				require("telescope.builtin").lsp_references()
+			end,
+			"[G]oto [R]eferences",
+		},
+		["<leader>rn"] = {
+			function()
+				require("nvchad.renamer").open()
+			end,
+			"LSP rename",
+		},
 		["<leader>op"] = { ":NvimTreeToggle <CR>", "Toggle nvim tree" },
-		["<leader>oP"] = { ":NvimTreeToggle <CR>", "Focus nvim tree" },
+		["<leader>oP"] = { ":NvimTreeFocus <CR>", "Focus nvim tree" },
 		["<leader>/"] = { ":Telescope live_grep<CR>", "Search keywords in project" },
 		["<leader>bi"] = { ":Telescope buffers<CR>", "Search buffers" },
 		["<leader>gg"] = { ":LazyGit<CR>", "Lazygit" },
