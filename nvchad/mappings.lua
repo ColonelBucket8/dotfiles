75cd201129508398418f3b88b44397b06918b740
ColonelBucket8 <freedomterran89@gmail.com>
2023-09-02 20:27:10 +0800

    Update lspconfig and mapping

--- nvchad/mappings.lua
+++ nvchad/mappings.lua
@@ -11,7 +11,7 @@ M.disabled = {
 
 M.general = {
 	n = {
-		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		-- [";"] = { ":", "enter command mode", opts = { nowait = true } },
 	},
 }
 
